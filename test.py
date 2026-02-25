42fdcbf4ff737e706fe0c59f2fb3774dd9be75d2
mlunkers <hs1145141919810@outlook.com>
2026-02-25 21:54:55 +0800

    Add files via upload
    Most of the issues have been resolved, and it can now be used for personal registration without errors (most likely).

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def create_user_name():
                 continue
             else:
                 break
-
+#收集用户姓氏
     while True:
         last_name = input('输入名字:')
         if last_name == '':
@@ -27,7 +27,7 @@ def create_user_name():
         else:
             last_name = last_name.strip()
             break
-
+#收集用户名字
     if first_name:
         user_name = first_name + last_name
         user_name = user_name.title()
@@ -36,9 +36,10 @@ def create_user_name():
         user_name = last_name
         user_name = user_name.title()
         return user_name
-
-def create_user_information(b=None,a=None):
+#合并为用户名
+def create_user_information(b=None,a=None,c=None):
     alpbhaet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    alpbhaet_copy = [i.upper() for i in alpbhaet]
     numbers = ['0','1','2','3','4','5','6','7','8','9']
     gender_choose = ('男','女')
     day_number = list(range(1,32))
@@ -55,22 +56,28 @@ def create_user_information(b=None,a=None):
                 break
         if b == False:
             for value in year:
-                if value in alpbhaet:
-                    a = True
+                if ' ' not in year and value not in alpbhaet and value not in alpbhaet_copy and value not in numbers:
+                    c = True
                     break
-            if a == True and ' ' not in year:
-                print('不能输入字母')
-                a = None
-                b = None
-                continue
-            elif ' ' in year:
-                print('不能输入空格')
+            if c == True:
+                print('不支持的语言或符号')
+                c = None
                 b = None
                 continue
             else:
-                print('不支持的语言或符号')
-                b = None
-                continue
+                for value in year:
+                    if ' ' not in year and value in alpbhaet or value in alpbhaet_copy:
+                        a = True
+                        break
+                if a == True:
+                    print('不能输入字母')
+                    a = None
+                    b = None
+                    continue
+                elif ' ' in year:
+                    print('不能输入空格')
+                    b = None
+                    continue
         elif b == None :
             year = int(year)
             if year > 2026 or year < 1920:
@@ -78,54 +85,92 @@ def create_user_information(b=None,a=None):
                 continue
             else:
                 break
-
+#收集用户出生年份
     while True:
         month = input('请输入你的出生月份')
         if month == '':
             print('月份不能为空')
             continue
-        if month.strip() == '':
-            print('不能输入空白')
-            continue
-        elif month != '':
+        for value in month:
+            if value not in numbers:
+                a = True
+                break
+        if a == True:
             for value in month:
-                a = (value in alpbhaet)
-            if a == True:
-                print('不能输入字母')
+                if ' ' not in month and value not in alpbhaet and value not in alpbhaet_copy and value not in numbers:
+                    c = True
+                    break
+            if c == True:
+                print('未知的字符')
+                c = None
+                a = None
                 continue
             else:
-                month = int(month)
+                for number in month:
+                    if ' ' not in month and number in alpbhaet or number in alpbhaet_copy:
+                        b = True
+                        break
+                if ' ' in month:
+                    print('不能输入空格')
+                    a = None
+                    continue
+                elif b == True:
+                    print('不能输入字母')
+                    a = None
+                    b = None
+                    continue
+        elif a == None:
+            month = int(month)
             if month not in mouth_number_2 + mouth_number_3:
                 print('请输入有效月份')
                 continue
             else:
                 break
-
+#收集用户出生月份
     while True:
         day = input('请输入出生日')
         if day == '':
             print('出生日不能为空')
             continue
-        if day.strip() == '':
-            print('不能输入空白')
-            continue
-        elif day != '':
-            for value in day:
-                a = (value in alpbhaet)
-            if a == True:
-                print('不能输入字母')
-                continue
-            else:
-                day = int(day)
-            if month in mouth_number_2 and day not in day_number:
-                print('请输入有效日期：')
-                continue
-            elif month in mouth_number_3 and day not in day_number[:-1]:
-                print('请输入有效日期：')
-                continue
-            else:
+        for value in day:
+            if value not in numbers:
+                a = False
                 break
-
+        if a == False:
+            for value in day:
+                if ' ' not in day and value not in alpbhaet and value not in alpbhaet_copy and value not in numbers:
+                    c = True
+                    break
+            if c == True:
+                print('不能输入符号或语言')
+                a = None
+                c = None
+                continue
+            else:
+                for value in day:
+                    if ' ' not in day and value in alpbhaet or a in alpbhaet_copy:
+                        b = True
+                        break
+                if b == True:
+                    print('不能输入字母')
+                    a = None
+                    b = None
+                    continue
+                elif ' ' in day:
+                    print('不能输入空格')
+                    a = None
+                    continue
+        elif a == None:
+            day = int(day)
+        if month in mouth_number_2 and day not in day_number:
+            print('请输入有效日期')
+            continue
+        elif month in mouth_number_3 and day not in day_number[:-1]:
+            print('请输入有效日期')
+            continue
+        else:
+            break
+#收集用户出生日
     while True:
         gender = input('请选择您的性别（男/女）')
         if gender in gender_choose:
@@ -135,37 +180,49 @@ def create_user_information(b=None,a=None):
             continue
 
     return year, month, day, gender
-
+#收集用户性别
 def create_user_account():
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    alpbhaet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+    alpbhaet_copy = [i.upper() for i in alpbhaet]
     while True:
         account = input('请创建一个账号：')
+        for value in account:
+            if value not in numbers and value not in alpbhaet and value not in alpbhaet_copy and value != '_':
+                c = True
+                break
         if account == '':
             print('账号不能为空')
             continue
-        if account.strip() == '':
-            print('不能输入空白')
+        elif c == True:
+            print('账号只能包含字母和数字还有下划线')
+            c = None
             continue
-        elif account != '':
+        else:
             if len(account) < 6 or len(account) > 18:
                 print('账号长度限制')
                 continue
             else:
                 break
-
+#创建账号
     while True:
         code = input('请创建一个密码')
         if code == '':
             print('密码不能为空')
             continue
-        if code.strip() == '':
-            print('不能输入空白')
-            continue
-        elif code != '':
+        for value in account:
+            if value not in numbers and value not in alpbhaet and value not in alpbhaet_copy and value != '_':
+                c = True
+                break
+        if c == True:
+            print('密码只能包含字母数字和下划线')
+        else:
             if len(code) < 6 or len(code) > 18:
                 print('密码长度限制')
                 continue
             else:
                 break
-
+#创建密码
     return account,code
 
